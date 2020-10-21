a689d9e8409f8df48842d3e83fbaf175200cd651
acra5y <oli.schmelzle@gmail.com>
2020-10-21 14:46:41 +0200

    Ensure right amount of arguments is passed

--- app/src/main/java/imageconverter/ArgumentsParser.java
+++ app/src/main/java/imageconverter/ArgumentsParser.java
@@ -5,6 +5,9 @@ import imageconverter.model.*;
 
 public class ArgumentsParser {
     public static Arguments parse(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Must specify exactly two arguments: input location and output location");
+        }
         return new Arguments(new InputLocation(args[0]), new OutputLocation(args[1]));
     }
 }
--- app/src/test/java/imageconverter/ArgumentsParserTest.java
+++ app/src/test/java/imageconverter/ArgumentsParserTest.java
@@ -5,7 +5,8 @@ import static org.junit.Assert.*;
 import imageconverter.model.*;
 
 public class ArgumentsParserTest {
-    @Test public void testParseArguments_success() {
+    @Test
+    public void testParseArguments_success() {
         String inputLocation = "inputLocation";
         String outputLocation = "outputLocation";
         String[] args = {inputLocation, outputLocation};
@@ -14,5 +15,19 @@ public class ArgumentsParserTest {
 
         assertEquals("Should create Arguments with correct input location", inputLocation, result.getInputLocation().getValue());
         assertEquals("Should create Arguments with correct output location", outputLocation, result.getOutputLocation().getValue());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseArguments_notEnoughArguments() {
+        String[] args = {"inputLocation"};
+
+        Arguments result = ArgumentsParser.parse(args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testParseArguments_tooManyArguments() {
+        String[] args = {"one", "two", "three"};
+
+        Arguments result = ArgumentsParser.parse(args);
     }
 }
