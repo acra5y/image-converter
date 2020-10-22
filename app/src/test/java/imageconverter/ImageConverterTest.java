a9cb80ab2d41f6d6c5ac9b8b2c9518757dd6fbee
acra5y <oli.schmelzle@gmail.com>
2020-10-22 13:41:56 +0200

    Test creating files

---
+++ app/src/test/java/imageconverter/ImageConverterTest.java
@@ -0,0 +1,35 @@
+package imageconverter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+import java.net.URL;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import org.apache.batik.transcoder.TranscoderException;
+import imageconverter.model.*;
+
+public class ImageConverterTest {
+    private URL getResource(String fileName) {
+        return ImageConverterTest.class.getClassLoader().getResource(fileName);
+    }
+
+    @Test
+    public void testConvert() throws URISyntaxException, IOException, TranscoderException {
+        URL resourceToConvert = this.getResource("test.svg");
+        String expectedFileName = "test.png";
+
+        assumeTrue(expectedFileName + " should not exist before converting", this.getResource(expectedFileName) == null);
+
+        String inputPath = resourceToConvert.getPath();
+        Arguments arguments = new Arguments(
+            new InputLocation(inputPath),
+            new OutputLocation(inputPath.replace(".svg", ".png"))
+        );
+
+        ImageConverter.convertImage(arguments);
+
+        assertNotNull(expectedFileName + " should have been created", this.getResource(expectedFileName));
+    }
+}
